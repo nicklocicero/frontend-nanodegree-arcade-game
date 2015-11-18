86e475625528d0d96dd51780ad588126e2d657da
nicklocicero <nicklocicero@gmail.com>
2015-11-17 17:15:26 -0800

    start

--- js/engine.js
+++ js/engine.js
@@ -14,19 +14,28 @@
  * a little simpler to work with.
  */
 
+// Define Engine class
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
      */
+     // set variables starting with doc as the global document
     var doc = global.document,
+        // win as the global window the window for the document
         win = global.window,
+        // adds a canvas element to this scope
         canvas = doc.createElement('canvas'),
+        // sets the canvas to be 2d type
         ctx = canvas.getContext('2d'),
+        // creates variable lastTime
         lastTime;
 
+    // gives canvas a width variable of 505
     canvas.width = 505;
+    // gives canvas a height variable of 606
     canvas.height = 606;
+    // appends the canvas to body
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -39,7 +48,12 @@ var Engine = (function(global) {
          * would be the same for everyone (regardless of how fast their
          * computer is) - hurray time!
          */
+
+         // Set now to number of milliseconds since
+         // 1 January 1970 00:00:00 UTC
         var now = Date.now(),
+            // Set dt the difference between now and lastTime
+            // Divide by 1000.0 to make dt a number in seconds
             dt = (now - lastTime) / 1000.0;
 
         /* Call our update/render functions, pass along the time delta to
@@ -64,8 +78,11 @@ var Engine = (function(global) {
      * game loop.
      */
     function init() {
+        // reset all values of the elements in the DOM
         reset();
+        // Create lastTime variable to be the currect time in milliseconds
         lastTime = Date.now();
+        // call main() function
         main();
     }
 
@@ -78,7 +95,9 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
+     // Used by game loop to update all entities
     function update(dt) {
+        // updates all entities
         updateEntities(dt);
         // checkCollisions();
     }
@@ -90,10 +109,14 @@ var Engine = (function(global) {
      * the data/properties related to  the object. Do your drawing in your
      * render methods.
      */
+     // combines updates for player and enemies
     function updateEntities(dt) {
+        // updates all enemies by looping through the array and calling
+        // each enemy's update method
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
+        // calls player's update method
         player.update();
     }
 
@@ -103,20 +126,25 @@ var Engine = (function(global) {
      * they are flipbooks creating the illusion of animation but in reality
      * they are just drawing the entire screen over and over.
      */
+     // function expression for render
     function render() {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
+
         var rowImages = [
-                'images/water-block.png',   // Top row is water
+                'images/water.png',   // Top row is water
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
+            // defines number of rows
             numRows = 6,
+            // number of columns
             numCols = 5,
+            // creates row and column variables
             row, col;
 
         /* Loop through the number of rows and columns we've defined above
@@ -132,11 +160,12 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
+                 // uses get to access cached images and draws them on canvas
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
         }
 
-
+        // renders enemies and player
         renderEntities();
     }
 
@@ -144,14 +173,17 @@ var Engine = (function(global) {
      * tick. It's purpose is to then call the render functions you have defined
      * on your enemy and player entities within app.js
      */
+     // creates renderEntities function expression
     function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        // renders all entities
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
+        // renders player
         player.render();
     }
 
@@ -167,13 +199,18 @@ var Engine = (function(global) {
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
      */
+
+    // loads images into game
     Resources.load([
         'images/stone-block.png',
-        'images/water-block.png',
+        'images/water.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png'
     ]);
+
+    // init function is passed into onReady, if it returns true, the game is
+    // cached and loaded, and the game starts
     Resources.onReady(init);
 
     /* Assign the canvas' context object to the global variable (the window
