1605d6691af555a9b460d8fc5e18635dbd996a70
nicklocicero <nicklocicero@gmail.com>
2015-11-20 00:01:28 -0800

    jshinted and beautified

--- js/app.js
+++ js/app.js
@@ -1,42 +1,30 @@
-// Enemies our player must avoid
+// Enemies our player must avoid.
 var Enemy = function(bugLane) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
-
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
+  // Set evil character's image.
   this.sprite = 'images/enemy-bug.png';
+  // Initiate evil off screen.
   this.x = -101;
+  // Give evil a lane.
   this.y = bugLane; // 51 // 134, 217
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position.
+// "dt" is a time delta between ticks.
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  // Give bug a random speed.
+  // Give evil a random speed.
   this.speed = [Math.floor((Math.random() * 3)) + 1];
   if (this.x < 505) {
     this.x += (dt * 275) * this.speed;
   } else {
-    // put bug back
+    // Put evil back.
     this.x = -101;
-    // give the bug a random lane
-    // this.y = [51, 134, 217][Math.floor((Math.random()+1)*3)]
   }
-
 };
 
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen.
 Enemy.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 var Player = function() {
   this.sprite = 'images/char-boy.png';
@@ -44,12 +32,9 @@ var Player = function() {
   this.y = 300;
 };
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the player's position.
 Player.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
+  // Unused.
 };
 
 Player.prototype.handleInput = function(input) {
@@ -75,35 +60,36 @@ Player.prototype.handleInput = function(input) {
   }
 };
 
-// Draw the player on the screen, required method for game
+// Draw the player on the screen.
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// Bring evil into form.
 var enemy1 = new Enemy(51);
 var enemy2 = new Enemy(134);
 var enemy3 = new Enemy(217);
+// Combine evil.
 var allEnemies = [enemy1, enemy2, enemy3];
-// Place the player object in a variable called player
+// Bring player into form.
 var player = new Player();
 
+// Check if evil has brought hell to player, forsaking his life.
 function checkCollisions() {
   allEnemies.forEach(function(enemy) {
-    if ((-10 < enemy.x - player.x && enemy.x - player.x < 10)
-           && (-10 < enemy.y - player.y && enemy.y - player.y < 10)) {
+    if ((-10 < enemy.x - player.x && enemy.x - player.x < 10) &&
+        (-10 < enemy.y - player.y && enemy.y - player.y < 10)) {
+      // Rise player, you are a phoenix, anew.
       player.x = 200;
       player.y = 300;
     }
   });
-};
+}
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and sends the keys to the player.
 document.addEventListener('keyup', function(e) {
   var allowedKeys = {
     37: 'left',
